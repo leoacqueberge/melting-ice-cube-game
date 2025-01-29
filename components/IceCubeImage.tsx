891683bfed61f7e77e0d62161232d3cf71f574d5
leoacqueberge <132593994+leoacqueberge@users.noreply.github.com>
2025-01-29 23:58:52 +0100

    Update IceCubeImage to use local SVG file

--- components/IceCubeImage.tsx
+++ components/IceCubeImage.tsx
@@ -1,22 +1,14 @@
-// Place your SVG code here
+import Image from 'next/image';
+
 export default function IceCubeImage() {
   return (
-    <svg viewBox="0 0 100 100" className="w-full h-full fill-blue-300 stroke-blue-400">
-      {/* Paste your SVG content here */}
-      <path
-        d="M20,30 L80,30 L80,90 L20,90 Z"
-        strokeWidth="2"
-      />
-      <path
-        d="M20,30 L35,15 L95,15 L80,30 Z"
-        strokeWidth="2"
-        className="fill-blue-200"
-      />
-      <path
-        d="M80,30 L95,15 L95,75 L80,90 Z"
-        strokeWidth="2"
-        className="fill-blue-400"
-      />
-    </svg>
+    <Image
+      src="/components/icecube.svg"
+      alt="Ice Cube"
+      width={128}
+      height={128}
+      className="w-full h-full"
+      priority
+    />
   );
 }
